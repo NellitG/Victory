770a316d03fb4c9e24d8099d38a97cae8a542ad0
NellitG <kamaunelson157@gmail.com>
2025-03-04 13:38:12 +0300

    added file path for other dashboards

--- club_management/urls.py
+++ club_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login_view, logout_view, dashboard
+from .views import register, login_view, logout_view, dashboard, patron_dashboard, student_dashboard
 from club_management.models import User
 
 
@@ -8,4 +8,8 @@ urlpatterns = [
     path("register/", register, name="signup"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path("admin_dashboard/", dashboard, name="admin_dashboard"),
+    path("patron_dashboard/", dashboard, name="patron_dashboard"),
+    path("student_dashboard/", dashboard, name="student_dashboard"),
+
 ]
