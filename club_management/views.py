02b5ed75ba3c16e38f105f542d9afd855dfca5d2
NellitG <kamaunelson157@gmail.com>
2025-03-04 14:35:51 +0300

    crud operations and memberships

--- club_management/views.py
+++ club_management/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Club, Membership
+from .forms import ClubForm, MembershipForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
@@ -60,3 +63,70 @@ def student_dashboard(request):
 @login_required(login_url='/login/')  # Redirect to login page if not logged in
 def dashboard(request):
     return render(request, 'club_management/dashboard.html')
+
+#List all clubs
+@login_required
+def club_list(request):
+    clubs = Club.objects.all()
+    return render(request, 'club_management/club_list.html', {'clubs': clubs})
+
+#Create a new club
+@login_required
+def club_create(request):
+    if request.method == 'POST':
+        form = ClubForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('club_list')
+    else:
+        form = ClubForm()
+    return render(request, 'club_management/club_form.html', {'form': form})
+
+#Update an exxisting club
+@login_required
+def club_update(request, pk):
+    club = get_object_or_404(Club, pk=pk)
+    if request.method == 'POST':
+        form = ClubForm(request.POST, instance=club)
+        if form.is_valid():
+            form.save()
+            return redirect('club_list')
+    else:
+        form = ClubForm(instance=club)
+    return render(request, 'club_management/club_form.html', {'form': form})
+
+#Delete an existing club
+@login_required
+def club_delete(request, pk):
+    club = get_object_or_404(Club, pk=pk)
+    if request.method == 'POST':
+        club.delete()
+        return redirect('club_list')
+    return render(request, 'club_management/club_confirm_delete.html', {'club': club})
+
+#List all memberships
+@login_required
+def membership_list(request):
+    memberships = Membership.objects.all()
+    return render(request, 'club_management/membership_list.html', {'memberships': memberships})
+
+#Create a new membership
+@login_required
+def membership_create(request):
+    if request.method == 'POST':
+        form = MembershipForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('membership_list')
+    else:
+        form = MembershipForm()
+    return render(request, 'club_management/membership_form.html', {'form': form})
+
+#Delete an existing membership
+@login_required
+def membership_delete(request, pk):
+    membership = get_object_or_404(Membership, pk=pk)
+    if request.method == 'POST':
+        membership.delete()
+        return redirect('membership_list')
+    return render(request, 'club_management/membership_confirm_delete.html', {'membership': membership})
